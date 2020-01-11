0cf265efb3617c77cbea57891cc78d91c2b233a8
reimuzerg <djh458@stern.nyu.edu>
2020-01-11 00:06:56 -0500

    added plotting to __all__ and added __version__

--- __init__.py
+++ __init__.py
@@ -4,6 +4,11 @@
 #
 # 01-10-2020
 #
-# initial creation. disable from shizuka import * (not much here anyways).
+# initial creation. add __doc__ for package level, __all__, and version.
 #
-__all__ = []
+__doc__ = """
+simplifying the analysis of statistical model performance by providing an
+abstracting layer over the standard python scientific computing stack.
+"""
+__all__ = ["plotting"]
+__version__ = "0.0.1"
