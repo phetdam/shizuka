4eaa4f8a929bfe3c2a975ea57942937004071c44
reimuzerg <djh458@stern.nyu.edu>
2020-01-11 21:35:32 -0500

    moved __init__.py to new package directory

---
+++ shizuka/__init__.py
@@ -0,0 +1,17 @@
+# __init__.py
+#
+# Changelog:
+#
+# 01-10-2020
+#
+# initial creation. add __doc__ for package level, __all__, and version.
+#
+__doc__ = """
+simplifying the analysis of statistical model performance by providing an
+abstracting layer over the standard python scientific computing stack.
+
+requires sklearn >=0.22.
+"""
+__all__ = ["plotting"]
+__version__ = "0.0.1"
+# check dependencies
