5e584f8fb7dde9e76dea89315989c7bb4e8c8f9b
reimuzerg <djh458@stern.nyu.edu>
2020-06-24 17:36:36 -0400

    added utility function module with functions to determine if an estimator is compatible or quasi-scikit-learn compatible, as will be defined in doc/source/model_compat.rst

---
+++ shizuka/utils.py
@@ -0,0 +1,88 @@
+# contains various utility functions
+#
+# Changelog:
+#
+# 06-24-2020
+#
+# initial creation. added functions for checking if an estimator is compatible
+# with the shizuka API or if the estimator is quasi-scikit-learn compatible.
+
+from inspect import getfullargspec
+from sys import stderr
+
+__doc__ = """Contains utility functions used throughout the package."""
+
+def is_compat(est):
+    """Determines if an estimator is compatible with the ``shizuka`` API.
+
+    The exact notion of "compatibility" is covered in 
+    :doc:`../doc/source/model_compat`. Please read that page for details.
+
+    .. note::
+
+       If ``est`` is ``None``, then ``False`` will be silently returned.
+
+    :param est: Estimator instance. Must be a concrete class instance, not a
+        function.
+    :type est: object
+    :returns: ``True`` if ``est`` is compatible, ``False`` otherwise.
+    :rtype: bool
+    """
+    # compatible if and only if not None and has a get_params method
+    if (est is not None) and hasattr(est, "get_params") and \
+       hasattr(est.get_params, "__call__"):
+        # get_params must take exactly 0 positional arguments
+        _argspec = getfullargspec(est.get_params)
+        if len(_argspec.args) == len(_argspec.defaults): return True
+    return False
+
+def is_skl_compat(est):
+    """Determines if an estiamtor is quasi-scikit-learn compatible.
+
+    The exact notion of "quasi-scikit-learn compatibility", which is a rather
+    loose definition, is covered in :doc:`../doc/source/model_compat`. Please
+    read that page for details.
+
+    .. note::
+
+       Calls :func:`is_compat` internally, so will return ``False`` if ``est``
+       is ``None``.
+
+    :param est: Estimator instance. Must be a concrete class instance, not a
+        function.
+    ;type est: object
+    :returns: ``True`` if ``est`` is quasi-scikit-learn compatible, ``False``
+        otherwise.
+    :rtype: bool
+    """
+    # if not compatible, return False
+    if is_compat(est) == False: return False
+    # need to get all 3 boolean flags to be True in order to return True
+    fit_ok, predict_ok, score_ok = False, False, False
+    # check if fit method is implemented, with correct call signature
+    if hasattr(est, "fit") and hasattr(est.fit, "__call__"):
+        _argspec = getfullargspec(est.fit)
+        # must have exactly 2 positional args
+        if (len(_argspec.args) >= 2) and \
+           (len(_argspec.args) - len(_argspec.defaults) == 2):
+            fit_ok = True
+    # check if predict method is implemented, with correct call signature
+    if hasattr(est, "predict") and hasattr(est.predict, "__call__"):
+        _argspec = getfullargspec(est.predict)
+        # must have exactly 1 positional arg
+        if (len(_argspec.args) >= 1) and \
+           (len(_argspec.args) - len(_argspec.defaults) == 1):
+            predict_ok = True
+    # check if score method is implemented, with correct call signature
+    if hasattr(est, "score") and hasattr(est.score, "__call__"):
+        _argspec = getfullargspec(est.score)
+        # must have exactly 2 positional args
+        if (len(_argspec.args) >= 2) and \
+           (len(_argspec.args) - len(_argspec.defaults) == 2):
+            score_ok = True
+    # return True if and only if fit, predict, and score are all ok
+    if (fit_ok and predict_ok and score_ok): return True
+    return False
+
+if __name__ == "__main__":
+    print("{0}: do not run module as script.".format(__module__), file = stderr)
