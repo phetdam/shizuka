e1c433fb119d7afa762095e2f5df8a1a04460abc
reimuzerg <djh458@stern.nyu.edu>
2020-06-24 22:47:23 -0400

    finished configuring conf.py. using sphinx RTD theme and added delimiter check so that path appended to sys.path pointing to local package directory is valid

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
+# configuration file for the Sphinx documentation builder.
+#
+# modified by Derek Huang
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -8,28 +10,33 @@
 #
 # 06-24-2020
 #
-# initial creation. change log added.
+# initial creation. change log added. added autodoc, autosummary, intersphinx,
+# and guzzle theme configuration. changed theme to RTD theme + add delimiter
+# check for correct sys.path paths (import local build, not installed
+# site-packages build, which led to builds not being updated).
 
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
+import os
+from os.path import dirname, abspath
+import sys
+# change delimiter style based on whether we have NT or POSIX
+_delim = "/"
+if os.name == "nt": _delim = "\\"
+sys.path.insert(0, dirname(abspath(__file__)) + _delim + ".." + _delim + "..")
 
 # -- Project information -----------------------------------------------------
 
-project = 'shizuka'
-copyright = '2020, Derek Huang'
-author = 'Derek Huang'
+project = "shizuka"
+copyright = "2020, Derek Huang"
+author = "Derek Huang"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
-
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 
@@ -37,6 +44,9 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,15 +57,49 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# -- autodoc configuration ---------------------------------------------------
+
+# set default options for autodoc directives. include __repr__ special member
+# and any private members (names prepended with _), show class inheritance,
+# but leave out any class members that are not documented with a docstring.
+#
+# note: since ignore-module-all is not set, only the members in __all__ in
+# __init__.py will be looked for and their order will be maintained.
+autodoc_default_options = {
+    "members": True,
+    "private-members": True,
+    "show-inheritance": True,
+    "special-members": "__repr__"
+}
+
+# -- autosummary configuration -----------------------------------------------
+
+# set to True to generate stub files for any modules named in a file's
+# autosummary directive(s). so far, only index.rst should have autosummary.
+autosummary_generate = True
+
+# -- intersphinx configuration -----------------------------------------------
+
+# determines which external package documentations to link to. thanks to the
+# link here: https://gist.github.com/bskinn/0e164963428d4b51017cebdb6cda5209
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None),
+                       "matplotlib": ("https://matplotlib.org/", None),
+                       "numpy": ("https://numpy.org/doc/stable/", None),
+                       "pandas": ("https://pandas.pydata.org/docs/", None)}
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+# name of the HTML theme. note that we wouldn't need all this other stuff if
+# we were using a builtin theme like nature, etc.
+html_theme = "sphinx_rtd_theme"
+
+# add theme to extensions so sphinx can go find it
+extensions.append(html_theme)
+
+# HTML theme options
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
